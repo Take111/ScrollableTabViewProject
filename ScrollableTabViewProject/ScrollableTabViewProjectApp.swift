9d8c18f2faad367a659fc0de4662c9535e3148be
Take111 <maxmaxastea@gmail.com>
2021-08-28 16:36:25 +0900

    Initial Commit

---
+++ ScrollableTabViewProject/ScrollableTabViewProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollableTabViewProjectApp.swift
+//  ScrollableTabViewProject
+//
+//  Created by Chikato Takenouchi on 2021/08/28.
+//
+
+import SwiftUI
+
+@main
+struct ScrollableTabViewProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
